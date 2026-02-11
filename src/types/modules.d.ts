955c30d6f5f8ca8b0d280d3659c9ad73922bba11
Sidomulyo Advertising <sosmedsidomulyo@gmail.com>
2026-02-11 10:36:54 +0700

    perbaikan di workschedule dan absensi

---
+++ src/types/modules.d.ts
@@ -0,0 +1,56 @@
+declare module "./auth/RequireAuth" {
+  const RequireAuth: any;
+  export default RequireAuth;
+}
+
+declare module "./auth/AuthProvider" {
+  export const AuthProvider: any;
+  export const useAuth: any;
+  const AuthProviderDefault: any;
+  export default AuthProviderDefault;
+}
+
+declare module "./pages/Login" {
+  const Login: any;
+  export default Login;
+}
+
+declare module "./pages/Dashboard" {
+  const Dashboard: any;
+  export default Dashboard;
+}
+
+declare module "./pages/Absensi" {
+  const Absensi: any;
+  export default Absensi;
+}
+
+declare module "./pages/Jadwal" {
+  const Jadwal: any;
+  export default Jadwal;
+}
+
+declare module "./pages/Gaji" {
+  const Gaji: any;
+  export default Gaji;
+}
+
+declare module "./pages/Lembur" {
+  const Lembur: any;
+  export default Lembur;
+}
+
+declare module "./pages/Profile" {
+  const Profile: any;
+  export default Profile;
+}
+
+declare module "./pages/JobTodoAvailable" {
+  const JobTodoAvailable: any;
+  export default JobTodoAvailable;
+}
+
+declare module "../layout/MobileLayout" {
+  const MobileLayout: any;
+  export default MobileLayout;
+}
